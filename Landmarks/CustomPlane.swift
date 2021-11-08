60193b4c1d0fbe6b7d599c8ac72ac6e1ec1693ad
Christy Ye <eragon1731@gmail.com>
2021-11-08 14:35:28 -0800

    starting shift to realitykit

---
+++ Landmarks/CustomPlane.swift
@@ -0,0 +1,28 @@
+//
+//  CustomPlane.swift
+//  Landmarks
+//
+//  Created by Christy Ye on 11/8/21.
+//  Copyright © 2021 Sean Fraga. All rights reserved.
+//
+
+import Foundation
+import RealityKit
+import UIKit
+
+class CustomPlane: Entity, HasModel, HasCollision, HasAnchoring{
+	
+	required init(image: UIImage) {
+		super .init()
+		self.components[ModelComponent] = ModelComponent(
+			mesh: .generatePlane(width: 1, depth: 1),
+			materials: [SimpleMaterial(color: .init(patternImage: image), isMetallic: false)]
+				)
+				self.name = "custom_plane"
+	}
+	
+	required init() {
+		fatalError("init() has not been implemented")
+	}
+	
+}
---
+++ Landmarks/RealityView.swift
@@ -0,0 +1,17 @@
+//
+//  RealityView.swift
+//  Landmarks
+//
+//  Created by Christy Ye on 11/8/21.
+//  Copyright © 2021 Sean Fraga. All rights reserved.
+//
+
+import Foundation
+import RealityKit
+import UIKit
+import ARKit
+
+class RealityView: UIViewController, ARSessionDelegate {
+	
+	
+}
