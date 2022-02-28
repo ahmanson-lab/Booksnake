d869b8882ff38b5d7f5549c1ef354f24972cdbda
Henry Huang <henry2423@gmail.com>
2022-02-27 22:40:39 -0800

    Implement extension files

---
+++ Landmarks/Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Extensions.swift
+//  Landmarks
+//
+//  Created by Henry Huang on 2/27/22.
+//  Copyright © 2022 University of Southern California. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct LazyView<Content: View>: View {
+    let build: () -> Content
+    init(_ build: @autoclosure @escaping () -> Content) {
+        self.build = build
+    }
+    var body: Content {
+        build()
+    }
+}
+
+extension Collection {
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
