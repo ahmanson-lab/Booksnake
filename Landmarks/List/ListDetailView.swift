4654b832ce57e657d921d75ce40dd3492c922bdd
Henry Huang <henry2423@gmail.com>
2022-02-27 22:52:19 -0800

    Implement list in listView

---
+++ Landmarks/List/ListDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  ListDetailView.swift
+//  Landmarks
+//
+//  Created by Henry Huang on 2/27/22.
+//  Copyright © 2022 University of Southern California. All rights reserved.
+//
+
+import Foundation
