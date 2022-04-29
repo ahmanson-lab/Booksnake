a27669b66bbbb9c9527cf6d8cdabeec4b61b8438
Christy Ye <eragon1731@gmail.com>
2022-04-29 11:55:15 -0700

    ui updates and HDL catalogue

--- Landmarks/Search/CustomSearchMenu.swift
+++ Landmarks/Search/CustomSearchMenu.swift
@@ -55,20 +55,20 @@ struct CustomSearchMenu: View {
 						})
 				
 				//Huntington Catalogue
-//				NavigationLink(
-//					destination: CatalogSearchView(label: $label, title: "Huntington Digital Library", type: "HDL", instructions: "Search results are limited to Huntington Digital Library materials with an IIIF manifest, which Booksnake uses to add items.", filter: "/id/", fieldURL: ["https://hdl.huntington.org/digital/search/collection/p15150coll8!p15150coll4!p15150coll2!p15150coll3!p9539coll1!p16003coll13!p16003coll11!p16003coll14!p16003coll12!p16003coll4!p16003coll7!p16003coll10!p16003coll5!p16003coll18!p16003coll15!p15150coll5!p16003coll17!p16003coll2!p16003coll16!p16003coll6!p16003coll9/searchterm/", ""], delegate: delegate),
-//					label: {
-//						Text("Huntington Digital Library")
-//							.fontWeight(.bold)
-//							.multilineTextAlignment(.center)
-//							.background(color4
-//											.frame(width: 360 - 10, height: 400 / 3, alignment: .center)
-//											.cornerRadius(10.0))
-//											.padding(.all, 10.0)
-//							.font(.title)
-//							.foregroundColor(.white)
-//						   .frame(width: 360 - 30, height: 400 / 3, alignment: .center)
-//					})
+				NavigationLink(
+					destination: LazyView(CatalogSearchView(title: "Huntington Digital Library", type: "HDL", instructions: "Search results are limited to Huntington Digital Library materials with an IIIF manifest, which Booksnake uses to add items.", filter: "/id/", fieldURL: ["https://hdl.huntington.org/digital/search/collection/p15150coll8!p15150coll4!p15150coll2!p15150coll3!p9539coll1!p16003coll13!p16003coll11!p16003coll14!p16003coll12!p16003coll4!p16003coll7!p16003coll10!p16003coll5!p16003coll18!p16003coll15!p15150coll5!p16003coll17!p16003coll2!p16003coll16!p16003coll6!p16003coll9/searchterm/", ""], delegate: delegate)),
+					label: {
+						Text("Huntington Digital Library")
+							.fontWeight(.bold)
+							.multilineTextAlignment(.center)
+							.background(color4
+											.frame(width: 360 - 10, height: 400 / 3, alignment: .center)
+											.cornerRadius(10.0))
+											.padding(.all, 10.0)
+							.font(.title)
+							.foregroundColor(.white)
+						   .frame(width: 360 - 30, height: 400 / 3, alignment: .center)
+					})
 			}
 		}
 	}
