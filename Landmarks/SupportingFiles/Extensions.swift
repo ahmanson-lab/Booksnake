420c03f91188620cfc4fa6bcfe1c6d815e3e68df
Henry Huang <henry2423@gmail.com>
2022-06-20 11:59:31 -0700

    [Feat] Add directional buttons for TextFields

--- Landmarks/SupportingFiles/Extensions.swift
+++ Landmarks/SupportingFiles/Extensions.swift
@@ -87,3 +87,19 @@ struct CustomNavigationView<Content: View>: View {
 		.frame(alignment: .topLeading)
 	}
 }
+
+extension CaseIterable where Self: Equatable, AllCases: BidirectionalCollection {
+    func previous() -> Self {
+        let all = Self.allCases
+        let idx = all.firstIndex(of: self)!
+        let previous = all.index(before: idx)
+        return all[previous < all.startIndex ? all.index(before: all.endIndex) : previous]
+    }
+
+    func next() -> Self {
+        let all = Self.allCases
+        let idx = all.firstIndex(of: self)!
+        let next = all.index(after: idx)
+        return all[next == all.endIndex ? all.startIndex : next]
+    }
+}
